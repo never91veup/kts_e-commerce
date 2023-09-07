eded0eec4d2ea8220bfae5cba5a4783009d860e2
Владислав Петров <vp.vlad00@mail.ru>
2023-09-07 14:36:39 +0300

    config files

---
+++ src/components/Button/Button.tsx
@@ -0,0 +1,13 @@
+import './Button.scss';
+
+const Button = () => {
+    return (
+        <div className="container">
+            <div className="container-color">
+                Button
+            </div>
+        </div>
+    );
+};
+
+export default Button;
