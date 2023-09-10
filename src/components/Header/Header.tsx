730ea340acebe6962a8d7428dcf2e6a76c038424
Владислав Петров <vp.vlad00@mail.ru>
2023-09-10 18:02:13 +0300

    add active route on NavLink

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import {NavLink, useLocation} from 'react-router-dom';
 import { uid } from 'uid';
 import Text from "../Text";
 import BagIcon from "../icons/BagIcon";
@@ -9,31 +10,36 @@ import UserIcon from "../icons/UserIcon";
 import styles from  "./Header.module.scss";
 
 const Header: React.FC = () => {
-    const NavHeaders: string[] = ["Products", "Categories", "About us"];
+  const NavHeaders: string[] = ["Products", "Categories", "About us"];
+  const location = useLocation();
 
-    return (
-        <div className={styles.header}>
-            <div className={styles.headerContent}>
-                <div className={styles.left}>
-                    <LogoIcon width="42" height="42" />
-                    <LalasiaIcon width="77" height="20" />
-                </div>
-                <div className={styles.center}>
-                    <ul className={styles.headerList}>
-                        {NavHeaders.map((item: string) => (
-                            <li key={uid()} className={styles.headerListItem}>
-                                <Text tag='div' weight="medium" view="p-18" maxLines={1}>{item}</Text>
-                            </li>
-                        ))}
-                    </ul>
-                </div>
-                <div className={styles.right}>
-                    <BagIcon className={stl.cp} width="30" height="30" />
-                    <UserIcon className={stl.cp} width="30" height="30" />
-                </div>
-            </div>
+  return (
+    <div className={styles.header}>
+      <div className={styles.headerContent}>
+        <div className={styles.left}>
+          <LogoIcon width="42" height="42" />
+          <LalasiaIcon width="77" height="20" />
         </div>
-    );
+        <div className={styles.center}>
+          <ul className={styles.headerList}>
+            {NavHeaders.map((item: string) => (
+              <NavLink
+                className={`${styles.headerListItem} ${location.pathname === `/${item.toLowerCase().split(" ").join("-")}` ? styles.active : ''}`}
+                key={uid()}
+                to={`/${item.toLowerCase().split(" ").join("-")}`}
+              >
+                <Text tag='div' weight="medium" view="p-18" maxLines={1}>{item}</Text>
+              </NavLink>
+            ))}
+          </ul>
+        </div>
+        <div className={styles.right}>
+          <BagIcon className={stl.cp} width="30" height="30" />
+          <UserIcon className={stl.cp} width="30" height="30" />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Header;
---
+++ src/pages/categories/CategoriesPage.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+// import styles from "./CategoriesPage.module.scss";
+
+const CategoriesPage: React.FC = () => {
+  return (
+    <div>
+      Categories
+    </div>
+  );
+};
+
+export default CategoriesPage;
